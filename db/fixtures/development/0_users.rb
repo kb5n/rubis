44f05789cefa445975eb406de9cf729aa684de7c
Shu Ozawa <shu@ozawa.tech>
2018-06-22 09:20:17 +0900

    パスワードを'test'に変更

--- db/fixtures/development/0_users.rb
+++ db/fixtures/development/0_users.rb
@@ -2,6 +2,6 @@ User.seed do |s|
   s.id = 1
   s.name = "Administrator"
   s.email = "admin@example.com"
-  s.crypted_password = '$2a$10$/aDFQVTtyLbC/gBL.JW1nuM2sZCJ7oHpz/m5txggSwQVMsoCjtbK6'
-  s.salt = 's5iDUFCErCjbt7pD5rkQ'
+  s.crypted_password = '$2a$10$jaTtxhcH1zk//NLYpHOhuOet3EZzqWdWAHtDgaXmmjAoFKJJ9BG4q'
+  s.salt = 'J2svwB3sv6SzggZ7ZdHn'
 end
