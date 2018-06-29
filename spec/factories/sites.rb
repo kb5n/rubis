9111c1a8d8abf44f0e605c8cfcbc6881130b657f
Shu Ozawa <shu@ozawa.tech>
2018-06-30 00:20:03 +0900

    add required column

--- spec/factories/sites.rb
+++ spec/factories/sites.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :site do
     title Faker::RockBand.name
     description Faker::Lorem.sentence
+    copyright Faker::Company.name
     started_at 1.year.ago
   end
 end
