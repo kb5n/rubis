9d68b3f733e9d52a8eb41f648ab61982ab34e848
Shu Ozawa <shu@ozawa.tech>
2018-07-11 10:43:34 +0900

    fix reference scope

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Rubis
     # when you request as ajax, set remote: true
     config.action_view.form_with_generates_remote_forms = false
 
-    Refile.secret_key = Rails.application.credentials.refile[:secret]
+    Refile.secret_key = credentials.refile[:secret]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
