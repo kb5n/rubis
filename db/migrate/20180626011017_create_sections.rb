9c2239cb77b079aa65a07c8e86ada7fda05500cf
Shu Ozawa <shu@ozawa.tech>
2018-07-11 09:44:04 +0900

    image upload with refile, nest sections into articles, fix columns for OpenGraph

--- db/migrate/20180626011017_create_sections.rb
+++ db/migrate/20180626011017_create_sections.rb
@@ -1,15 +1,23 @@
 class CreateSections < ActiveRecord::Migration[5.2]
+  # rubocop:disable Metrics/AbcSize
   def change
     create_table :sections do |t|
       t.references :article
       t.integer :content_type
-      t.string :title
       t.integer :sequence, null: false
+      t.string :title
       t.text :description
+      t.string :image_id
+      t.string :image_filename
+      t.string :image_content_size
+      t.string :image_content_type
       t.text :url
-      t.text :image_url
-      t.string :source
+      t.string :og_site_name
+      t.string :og_title
+      t.text :og_description
+      t.text :og_image
       t.timestamps
     end
   end
+  # rubocop:enable Metrics/AbcSize
 end
