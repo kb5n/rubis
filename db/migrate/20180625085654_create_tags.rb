b858657886e3dbe0a9ee4d610fb08c231d7d710e
Shu Ozawa <shu@ozawa.tech>
2018-08-13 16:30:51 +0900

    fix migration for mysql

--- db/migrate/20180625085654_create_tags.rb
+++ db/migrate/20180625085654_create_tags.rb
@@ -2,7 +2,7 @@ class CreateTags < ActiveRecord::Migration[5.2]
   def change
     create_table :tags do |t|
       t.string :name, null: false
-      t.string :identifier, null: false
+      t.string :identifier, limit: 128, null: false
       t.references :user
 
       t.timestamps
