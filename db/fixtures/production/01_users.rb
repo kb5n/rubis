6d110e36713ff9534aedd145a2206a348b9f749a
Shu Ozawa <shu@ozawa.tech>
2018-08-15 10:07:55 +0900

    stop using Faker on production fixture

--- db/fixtures/production/01_users.rb
+++ db/fixtures/production/01_users.rb
@@ -1,10 +1,10 @@
 User.seed do |s|
-  s.name = Faker::Name.name
+  s.name = 'Administrator'
   s.email = 'admin@example.com'
   s.identifier = 'admin'
   s.role_type = :admin
-  s.blog_title = Faker::RockBand.name
-  s.blog_description = Faker::Lorem.sentence
+  s.blog_title = 'rubis'
+  s.blog_description = 'this is an example blog'
   s.blog_started_at = DateTime.now
   s.crypted_password = '$2a$10$jaTtxhcH1zk//NLYpHOhuOet3EZzqWdWAHtDgaXmmjAoFKJJ9BG4q'
   s.salt = 'J2svwB3sv6SzggZ7ZdHn'
