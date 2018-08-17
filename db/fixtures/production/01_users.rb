9e54d7063101a73e4b3a2ff4b67826396c33c099
Shu Ozawa <shu@ozawa.tech>
2018-08-17 10:34:13 +0900

    use current instead of now

--- db/fixtures/production/01_users.rb
+++ db/fixtures/production/01_users.rb
@@ -5,7 +5,7 @@ User.seed do |s|
   s.role_type = :admin
   s.blog_title = 'rubis'
   s.blog_description = 'this is an example blog'
-  s.blog_started_at = DateTime.now
+  s.blog_started_at = DateTime.current
   s.crypted_password = '$2a$10$jaTtxhcH1zk//NLYpHOhuOet3EZzqWdWAHtDgaXmmjAoFKJJ9BG4q'
   s.salt = 'J2svwB3sv6SzggZ7ZdHn'
 end
