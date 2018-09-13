c1110a9e987850eed19e6b8fd31a651aef6ed668
Shu Ozawa <shu@ozawa.tech>
2018-09-13 23:48:54 +0900

    remove unnecessary method `all`

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find_by!(identifier: params[:identifier])
-    @articles = Article.opened.where(category_id: @category.id).all.order('published_at desc')
+    @articles = Article.opened.where(category_id: @category.id).order('published_at desc')
     render 'top/index'
   end
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find_by!(identifier: params[:identifier])
-    @articles = Article.opened.where(tags: { id: @tag.id }).joins(:tags).all.order('published_at desc')
+    @articles = Article.opened.where(tags: { id: @tag.id }).joins(:tags).order('published_at desc')
     render 'top/index'
   end
 end
